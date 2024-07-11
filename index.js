f23d221e406ee6c25d63cbc7125905f75bf357f4
CharityNambi05 <charity.nambi@icloud.com>
2024-07-11 15:58:10 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+
+let multiply = num1*num2
+
+let random =Math.round(Math.random() +1);
+
+let num3 =10;
+let num4 = 6;
+let mod = num3%num4;
+let max = 20;
+console.log (Math.max(5,10,15,20));
+
+
